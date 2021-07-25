1161068d656a6c6181a72988dc415ae43934f35e
dasauser <dasauser@gmail.com>
2021-07-25 20:53:43 +0300

    minor improvements

--- src/index.php
+++ src/index.php
@@ -15,18 +15,20 @@
 <div class="flex">
     <h2><?php echo date('H:i:s d.m.Y')?></h2>
     <h1>Welcome to web-app in Docker Container!</h1>
+    <div class="flex">
+        <?php
+        $mysqli = new mysqli("mysql", "admin", "admin", "base", 3306);
+        echo "MYSQL status: ";
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+        } else {
+            printf ("%s\n", $mysqli->stat());
+            $mysqli->close();
+        }
+        ?>
+    </div>
     <div>
         <?php phpinfo() ?>
-        <br>
-        <?php
-        var_dump(mysqli_connect(
-            'mysql',
-            'admin',
-            'admin',
-            'base',
-            3306
-        )->get_connection_stats());
-        ?>
     </div>
 </div>
 </body>
